35a03f93c8d8293c1ec99de77041b072ab2493ff
Vikas Kumar <vikasvq@gmail.com>
2026-06-13 15:02:38 +0530

    new code add in my branch

---
+++ src/Angular/PasswordGenerator.java
@@ -0,0 +1,21 @@
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final String CHAR_SET = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+        "abcdefghijklmnopqrstuvwxyz" +
+        "0123456789" +
+        "!@#$%^&*()-_=+";
+
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        for (int i = 0; i < 8; i++) {
+            int index = random.nextInt(CHAR_SET.length());
+            password.append(CHAR_SET.charAt(index));
+        }
+
+        System.out.println("Generated Password: " + password.toString());
+    }
+}
